6e5f408765e053a9f2d4c47e65cf4360dc563c6f
Sameera Wickramasekara <sameeraw@wso2.com>
2018-10-02 10:35:34 +0530

    Update the kubernetes cluster creation logic

--- cluster-destroy.sh
+++ cluster-destroy.sh
@@ -1,1 +1,25 @@
-echo "Cluster destory script executed !"
+#!bin/bash
+
+echo "Resource deletion script is being executed !"
+DIR=$2
+echo $DIR
+
+testplan_props=${DIR}/testplan-props.properties
+infra_props=${DIR}/infrastructure.properties
+
+#delete database
+db_identifier=`cat ${infra_props} | grep -w DatabaseName ${infra_props} | cut -d'=' -f2`
+aws rds delete-db-instance --db-instance-identifier "$db_identifier" --skip-final-snapshot
+echo "rds deletion triggered"
+
+delete_cluster=`cat ${testplan_props} | grep -w DeleteCluster ${testplan_props} | cut -d'=' -f2`
+if [ "$delete_cluster" == "TRUE" ];then
+	#delete cluster resources
+	cluster_name=`cat ${infra_props} | grep -w ClusterName ${infra_props} | cut -d'=' -f2`
+	aws cloudformation delete-stack --stack-name=EKS-$cluster_name-DefaultNodeGroup
+	aws cloudformation delete-stack --stack-name=EKS-$cluster_name-ControlPlane
+	aws cloudformation delete-stack --stack-name=EKS-$cluster_name-VPC
+	aws cloudformation delete-stack --stack-name=EKS-$cluster_name-ServiceRole
+	echo " cluster resources deletion triggered"
+fi
+
--- infra.sh
+++ infra.sh
@@ -1,39 +1,122 @@
 #!bin/bash
 
-kube_master=`echo $1 | awk -F= '{print $2}'`
-output_dir=`echo $2 | awk -F= '{print $2}'`
+output_dir=$2
+echo $output_dir
+
+cluster_name="wso2sp-cluster"
+retry_attempts=3
+config_file=~/.kube/config
+echo $retry_attempts
+
+
+while [ "$STATUS" != "ACTIVE" ] && [ $retry_attempts -gt 0 ]
+do
+    eksctl create cluster --name "$cluster_name" --region us-east-1 --nodes-max 3 --nodes-min 1 --node-type m5.large --zones=us-east-1a,us-east-1b,us-east-1d
+    if [ $? -ne 0 ]; then
+         echo "Waiting for service role.."
+         aws cloudformation wait stack-create-complete --stack-name=EKS-$cluster_name-ServiceRole
+         echo "Waiting for vpc.."
+         aws cloudformation wait stack-create-complete --stack-name=EKS-$cluster_name-VPC
+         echo "Waiting for Control Plane.."
+         aws cloudformation wait stack-create-complete --stack-name=EKS-$cluster_name-ControlPlane
+         echo "Waiting for node-group.."
+         aws cloudformation wait stack-create-complete --stack-name=EKS-$cluster_name-DefaultNodeGroup  
+    else
+        #if the cluster creation is succesful , any existing config files are removed
+        if [ -f "$config_file" ];then
+            rm $config_file
+        fi
+
+        #Configure the security group of nodes to allow traffic from outside
+        node_security_group=$(aws ec2 describe-security-groups --filter Name=tag:aws:cloudformation:logical-id,Values=NodeSecurityGroup --query="SecurityGroups[0].GroupId" --output=text)
+        aws ec2 authorize-security-group-ingress --group-id $node_security_group --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
+    fi
+    STATUS=$(aws eks describe-cluster --name $cluster_name --query="[cluster.status]" --output=text)  
+    echo "Status is "$STATUS
+    retry_attempts=$(($retry_attempts-1))
+    echo "attempts left : "$retry_attempts
+done
+
+#if the status is not active by this phase the cluster creation has failed, hence exiting the script in error state
+if [ "$STATUS" != "ACTIVE" ];then
+    echo "state is not active"
+    exit 1
+fi
+
+# Check if config file exists, if it does not exist create the config file
+if [ ! -f "$config_file" ];then
+    echo "config file does not exist"
+    eksctl utils write-kubeconfig --name $cluster_name --region us-east-1
+fi
 
 infra_properties=$output_dir/infrastructure.properties
+testplan_properties=$output_dir/testplan-props.properties
+
+db=`cat ${testplan_properties} | grep -w DBEngine ${testplan_properties} | cut -d'=' -f2`
+db_version=`cat ${testplan_properties} | grep -w DBEngineVersion ${testplan_properties} | cut -d'=' -f2`
+
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
 #database-name
 database_name=wso2sp-kubernetes-"$NEW_UUID"
 
-echo $kube_master
-echo $output_dir
-
-echo "KUBERNETES_MASTER=$kube_master" > $output_dir/k8s.properties
+# echo $kube_master
+# echo $output_dir
+# echo "KUBERNETES_MASTER=$kube_master" > $output_dir/k8s.properties
 
 ##create the database
-echo $output_dir
+
+if [ "$db" == "mysql" ];then
+
 aws rds create-db-instance --db-instance-identifier "$database_name" \
     --db-instance-class db.t2.medium \
-    --engine MySQL \
+    --engine "$db" \
     --allocated-storage 10 \
     --master-username masterawsuser \
     --master-user-password masteruserpassword \
-    --backup-retention-period 0
+    --backup-retention-period 0\
+    --engine-version "$db_version"
 
 db_port=3306
 
+elif [ "$db" == "sqlserver-se" ];then
+
+aws rds create-db-instance --db-instance-identifier "$database_name" \
+    --db-instance-class db.m4.large \
+    --engine "$db"  \
+    --allocated-storage 20  \
+    --master-username masterawsuser  \
+    --master-user-password masteruserpassword \
+    --backup-retention-period 0 \
+    --license-model license-included \
+    --engine-version "$db_version"
+
+db_port=1433
+
+elif [ "$db" == "oracle-se2" ];then 
+
+aws rds create-db-instance --db-instance-identifier "$database_name" \
+    --db-instance-class db.t2.medium  \
+    --engine "$db"  \
+    --allocated-storage 10  \
+    --master-username masterawsuser  \
+    --master-user-password masteruserpassword \
+    --backup-retention-period 0 \
+    --license-model license-included \
+    --engine-version "$db_version"
+
+db_port=1521
+fi
+
 #Wait for the database to become available
-aws rds wait     db-instance-available     --db-instance-identifier "$database_name"
+aws rds wait  db-instance-available  --db-instance-identifier "$database_name"
 #retrieve the database hostname
 echo "DatabasePort=$db_port" >> $output_dir/infrastructure.properties
 echo "DatabaseName=$database_name" >> $output_dir/infrastructure.properties
 echo "DatabaseHost="$(aws rds describe-db-instances --db-instance-identifier="$database_name" --query="[DBInstances][][Endpoint][].{Address:Address}" --output=text) >> $output_dir/infrastructure.properties
 echo "DBUsername=masterawsuser" >> $output_dir/infrastructure.properties
 echo "DBPassword=masteruserpassword" >> $output_dir/infrastructure.properties
+echo "ClusterName=$cluster_name" >> $output_dir/infrastructure.properties
 
 
 
